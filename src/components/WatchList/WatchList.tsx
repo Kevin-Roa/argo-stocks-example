385deb60c3f64691c4062034cd591da95ed9e801
saudbaig456 <saudsamibaig@gmail.com>
2023-03-30 17:53:43 -0500

    add sorting and app bar

--- src/components/WatchList/WatchList.tsx
+++ src/components/WatchList/WatchList.tsx
@@ -1,75 +1,99 @@
-import React, {useMemo} from "react";
-import MaterialReactTable, { type MRT_ColumnDef } from 'material-react-table';
-import { OpenDirOptions } from "fs";
+import React, { useMemo } from "react";
+import MaterialReactTable, { type MRT_ColumnDef } from "material-react-table";
+import { stocks } from "@/config";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 export default function WatchList(props: any) {
-  //TODO: get the stocks dynamically from index
-  const stocks = ["AAPL", "MSFT", "AAL", "ABC"];
-
-  //TODO: change everything except name to number data type
   type Stock = {
-    Name: string,
-    close: string,
-    date: string,
-    high: string,
-    low: string,
-    open: string,
-    volume: string
-  }
+    Name: string;
+    close: number;
+    date: number;
+    high: number;
+    low: number;
+    open: number;
+    volume: number;
+  };
 
-  //TODO: change data type before inserting to array for data attributes
   const arr: Stock[] = [];
   stocks.forEach((stock) => {
-    arr.push(props.data[stock].data[1200]);
+    const currStockData = props.data[stock].data[1200];
+    //convert to numbers so they can be sorted
+    let temp: Stock = {
+      Name: currStockData.Name,
+      close: Number(currStockData.close),
+      date: currStockData.date,
+      high: Number(currStockData.high),
+      low: Number(currStockData.low),
+      open: Number(currStockData.open),
+      volume: Number(currStockData.volume),
+    };
+    arr.push(temp);
   });
 
-  
   const columns = useMemo<MRT_ColumnDef<Stock>[]>(
     () => [
       {
-        accessorKey: 'Name',
-        header: 'Ticker',
+        accessorKey: "Name",
+        header: "Ticker",
         size: 50,
       },
       {
-        accessorKey: 'close',
-        header: 'Close',
+        accessorKey: "date",
+        header: "Date",
         size: 50,
       },
       {
-        accessorKey: 'date',
-        header: 'Date',
+        accessorKey: "open",
+        header: "Open",
         size: 50,
       },
       {
-        accessorKey: 'high',
-        header: 'High',
+        accessorKey: "high",
+        header: "High",
         size: 50,
       },
       {
-        accessorKey: 'low',
-        header: 'Low',
+        accessorKey: "low",
+        header: "Low",
         size: 50,
-      },  
-      {
-        accessorKey: 'open',
-        header: 'Open',
-        size: 50,
-
       },
       {
-        accessorKey: 'volume',
-        header: 'Volume',
+        accessorKey: "close",
+        header: "Close",
+        size: 50,
+      },
+      {
+        accessorKey: "volume",
+        header: "Volume",
         size: 50,
       },
     ],
-    [],
+    []
   );
 
   return (
-    <>
-      <div>WatchList</div>
-      <MaterialReactTable columns={columns} data={arr} initialState={{ density: 'compact'}}/>
-    </>
+    <div>
+      {/* replace with header component */}
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "white", color: "black" }}
+      >
+        <Toolbar>
+          <Box>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Watchlist
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <MaterialReactTable
+        columns={columns}
+        data={arr}
+        initialState={{ density: "compact" }}
+        muiSearchTextFieldProps={{
+          placeholder: "Search Stocks",
+        }}
+      />
+    </div>
   );
 }

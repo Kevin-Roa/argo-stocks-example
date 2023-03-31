8d32be258d4b8f99f9174cf5422983fb63dade7c
saudbaig456 <saudsamibaig@gmail.com>
2023-03-31 18:41:29 -0500

    refactor to use argo table component

--- src/components/WatchList/WatchList.tsx
+++ src/components/WatchList/WatchList.tsx
@@ -1,20 +1,21 @@
-import React, { useMemo } from "react";
-import MaterialReactTable, { type MRT_ColumnDef } from "material-react-table";
+import React from "react";
 import { stocks } from "@/config";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { Table } from "@utd-argo/ux-master-library";
+import { Column } from "@utd-argo/ux-master-library/components/table/Table";
 
 export default function WatchList(props: any) {
   type Stock = {
     Name: string;
     close: number;
-    date: number;
+    date: string;
     high: number;
     low: number;
     open: number;
     volume: number;
   };
 
-  const arr: Stock[] = [];
+  //table data
+  const data: Stock[] = [];
   stocks.forEach((stock) => {
     const currStockData = props.data[stock].data[1200];
     //convert to numbers so they can be sorted
@@ -27,73 +28,49 @@ export default function WatchList(props: any) {
       open: Number(currStockData.open),
       volume: Number(currStockData.volume),
     };
-    arr.push(temp);
+    data.push(temp);
   });
 
-  const columns = useMemo<MRT_ColumnDef<Stock>[]>(
-    () => [
-      {
-        accessorKey: "Name",
-        header: "Ticker",
-        size: 50,
-      },
-      {
-        accessorKey: "date",
-        header: "Date",
-        size: 50,
-      },
-      {
-        accessorKey: "open",
-        header: "Open",
-        size: 50,
-      },
-      {
-        accessorKey: "high",
-        header: "High",
-        size: 50,
-      },
-      {
-        accessorKey: "low",
-        header: "Low",
-        size: 50,
-      },
-      {
-        accessorKey: "close",
-        header: "Close",
-        size: 50,
-      },
-      {
-        accessorKey: "volume",
-        header: "Volume",
-        size: 50,
-      },
-    ],
-    []
-  );
-
+  //heading information
+  const columns: (Column & {size: number})[] = [
+    {
+      accessorKey: "Name",
+      header: "Ticker",
+      size: 50,
+    },
+    {
+      accessorKey: "date",
+      header: "Date",
+      size: 50,
+    },
+    {
+      accessorKey: "open",
+      header: "Open",
+      size: 50,
+    },
+    {
+      accessorKey: "high",
+      header: "High",
+      size: 50,
+    },
+    {
+      accessorKey: "low",
+      header: "Low",
+      size: 50,
+    },
+    {
+      accessorKey: "close",
+      header: "Close",
+      size: 50,
+    },
+    {
+      accessorKey: "volume",
+      header: "Volume",
+      size: 50,
+    },
+  ];
+  
   return (
-    <div>
-      {/* replace with header component */}
-      <AppBar
-        position="static"
-        sx={{ backgroundColor: "white", color: "black" }}
-      >
-        <Toolbar>
-          <Box>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Watchlist
-            </Typography>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <MaterialReactTable
-        columns={columns}
-        data={arr}
-        initialState={{ density: "compact" }}
-        muiSearchTextFieldProps={{
-          placeholder: "Search Stocks",
-        }}
-      />
-    </div>
+      <Table columns={columns} data={data} title="Watchlist" density="comfortable"/>
   );
 }
